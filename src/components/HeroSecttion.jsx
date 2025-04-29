eeea355419eeb7ea883ae9f4669b547c101e6e5d
Shuchi Yadav <shuchiyadav2307@gmail.com>
2025-04-29 21:31:37 +0530

    remove width in small device

--- src/components/HeroSecttion.jsx
+++ src/components/HeroSecttion.jsx
@@ -21,11 +21,11 @@ const HeroSecttion = () => {
             </a>
         </div>
         <div className='flex mt-10 justify-center'>
-            <video autoPlay loop muted className='rounded-lg w-1/2 sm:w-full border border-orange-700 shadow-orange-400 mx-2 my-4'>
+            <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'>
                 <source src={video1} type='video/mp4'></source>
                 Your browser does nott support the video tag.
             </video>
-            <video autoPlay loop muted className='rounded-lg w-1/2 sm:w-full border border-orange-700 shadow-orange-400 mx-2 my-4'>
+            <video autoPlay loop muted className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'>
                 <source src={video2} type='video/mp4'></source>
                 Your browser does nott support the video tag.
             </video>
